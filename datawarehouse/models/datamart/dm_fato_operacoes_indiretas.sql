1b0262edb3f1afab94098e70b562fb38e137a2e3
HemersonDouglas <seuemail@gmail.com>
2025-03-30 20:30:35 -0300

    feat: adicionar datamart e staging

---
+++ datawarehouse/models/datamart/dm_fato_operacoes_indiretas.sql
@@ -0,0 +1,28 @@
+with foperacoes_indiretas as (
+    select 
+        "data_da_Contratacao",
+        "cpf_cnpj",
+        "municipio_codigo",
+        "juros",
+        "prazo_carencia_meses",
+        "prazo_amortizacao_meses",
+        "valor_contratado_reais",
+        "valor_desembolsado_reais"
+    from {{ ref('stg_operacoes_indiretas') }}
+),
+
+renamed as (
+    select
+        cast("data_da_Contratacao" as date) as data_da_Contratacao,
+        cast("cpf_cnpj" as varchar(18)) as cnpj,
+        cast("municipio_codigo" as int) as municipio_codigo,
+        -- Substituir NULL por valor padrão (0 ou outro valor válido)
+        COALESCE(TRY_CAST("juros" AS decimal(10,2)), 0) AS juros,
+        COALESCE(TRY_CAST("prazo_carencia_meses" AS decimal(10,2)), 0) AS prazo_carencia_meses,
+        cast("prazo_amortizacao_meses" as int) as prazo_amortizacao_meses, 
+        COALESCE(TRY_CAST("valor_contratado_reais" as decimal(18,2 )), 0) as valor_Contratado_Reais,
+        COALESCE(TRY_CAST("valor_desembolsado_reais" as decimal(18,2 )), 0) as valor_Desembolsado_Reais
+    from foperacoes_indiretas
+)
+
+select * from renamed;
--- datawarehouse/models/staging/stg_operacoes_diretas.sql
+++ datawarehouse/models/staging/stg_operacoes_diretas.sql
@@ -1,6 +1,6 @@
 with source as (
     select
-        "data_da_Contratacao",
+        "data_da_contratacao",
         "cliente",
         "cnpj",
         "uf",
@@ -11,13 +11,15 @@ with source as (
         "produto",
         "juros",
         "prazo_carencia_meses",
-        "prazo_amortizacao_meses"
+        "prazo_amortizacao_meses",
+        "valor_contratado_reais",
+        "valor_desembolsado_reais"
     from {{ source('case-abc-bank', 'operacoes-diretas') }}
 ),
 
 renamed as (
     select
-         cast("data_da_Contratacao" as date) as data_da_Contratacao,
+        cast("data_da_Contratacao" as date) as data_da_Contratacao,
         cast("cliente" as nvarchar(255)) as cliente,
         cast("cnpj" as varchar(18)) as cnpj,
         cast("uf" as nvarchar(2)) as uf,
@@ -29,7 +31,9 @@ renamed as (
         -- Substituir NULL por valor padrão (0 ou outro valor válido)
         COALESCE(TRY_CAST("juros" AS decimal(10,2)), 0) AS juros,
         COALESCE(TRY_CAST("prazo_carencia_meses" AS decimal(10,2)), 0) AS prazo_carencia_meses,
-        cast("prazo_amortizacao_meses" as int) as prazo_amortizacao_meses  
+        cast("prazo_amortizacao_meses" as int) as prazo_amortizacao_meses, 
+        COALESCE(TRY_CAST("valor_contratado_reais" as decimal(18,2 )), 0) as valor_Contratado_Reais,
+        COALESCE(TRY_CAST("valor_desembolsado_reais" as decimal(18,2 )), 0) as valor_Desembolsado_Reais
     from source
 )
 
