00f68c7b64bac2e4e8f8a49f2f5d254fe41744b1
HemersonDouglas <seuemail@gmail.com>
2025-03-30 17:03:08 -0300

    feat: Tabela stg_operacoes_diretas criada

---
+++ datawarehouse/models/staging/stg_operacoes_diretas.sql
@@ -0,0 +1,39 @@
+with source as (
+    select
+        "data_da_Contratacao",
+        "cliente",
+        "cnpj",
+        "uf",
+        "municipio",
+        "municipio_codigo",
+        "modalidade_de_apoio",
+        "forma_de_apoio",
+        "produto",
+        "juros",
+        "prazo_carencia_meses",
+        "prazo_amortizacao_meses"
+    from {{ source('case-abc-bank', 'operacoes-diretas') }}
+),
+
+renamed as (
+    select
+         cast("data_da_Contratacao" as date) as data_da_Contratacao,
+        cast("cliente" as nvarchar(255)) as cliente,
+        cast("cnpj" as varchar(18)) as cnpj,
+        cast("uf" as nvarchar(2)) as uf,
+        cast("municipio" as nvarchar(255)) as municipio,
+        cast("municipio_codigo" as int) as municipio_codigo,
+        cast("modalidade_de_apoio" as nvarchar(255)) as modalidade_de_apoio,
+        cast("forma_de_apoio" as nvarchar(255)) as forma_de_apoio,
+        cast("produto" as nvarchar(255)) as produto,
+        -- Substituir NULL por valor padrão (0 ou outro valor válido)
+        COALESCE(TRY_CAST("juros" AS decimal(10,2)), 0) AS juros,
+        COALESCE(TRY_CAST("prazo_carencia_meses" AS decimal(10,2)), 0) AS prazo_carencia_meses,
+        cast("prazo_amortizacao_meses" as int) as prazo_amortizacao_meses  
+    from source
+)
+
+select * from renamed;
+
+
+
