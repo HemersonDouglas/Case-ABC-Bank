44f3125ebb86a3a77f77d5b860802efd935e9174
HemersonDouglas <seuemail@gmail.com>
2025-03-30 14:35:12 -0300

    feat: Dados inseridos no banco SQLServer

--- src/extract_load.py
+++ src/extract_load.py
@@ -1,20 +1,47 @@
 import pandas as pd
 import pyodbc
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 
+# Carregar variáveis de ambiente
 load_dotenv()
 
+# Obter as variáveis do .env
 server = os.getenv('server')
 user = os.getenv('user')
 password = os.getenv('password')
 database = os.getenv('database')
 
+# Caminho da pasta onde estão os CSVs
+csv_folder_path = r"C:\Users\ubicase01\Desktop\BASES\bases-operacaoes-case"  # Caminho ajustado para o seu diretório
+
+# Configuração da string de conexão para o SQL Server usando SQLAlchemy
+connection_string = f"mssql+pyodbc://{user}:{password}@{server}/{database}?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes&TrustServerCertificate=yes"
+
+# Conectar ao banco de dados SQL Server com SQLAlchemy
 try:
-    conn = pyodbc.connect(
-        f"DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={user};PWD={password}"
-    )
-    print("✅ Conexão bem-sucedida!")
-    conn.close()
+    engine = create_engine(connection_string)
+    print("✅ Conexão bem-sucedida ao SQL Server!")
+
+    # Percorrer todos os arquivos CSV na pasta
+    for csv_file in os.listdir(csv_folder_path):
+        if csv_file.endswith(".csv"):
+            csv_path = os.path.join(csv_folder_path, csv_file)
+            
+            # Carregar o arquivo CSV em um DataFrame do pandas
+            df = pd.read_csv(csv_path, delimiter=";")
+            print(f"✅ Arquivo CSV '{csv_file}' carregado com {df.shape[0]} linhas e {df.shape[1]} colunas.")
+            
+            # Definir o nome da tabela com base no nome do arquivo CSV
+            table_name = os.path.splitext(csv_file)[0]  # Usa o nome do arquivo (sem extensão) como nome da tabela
+
+            # Inserir os dados na tabela do SQL Server
+            df.to_sql(table_name, con=engine, schema='dbo', if_exists='replace', index=False)
+
+            print(f"✅ Dados inseridos com sucesso na tabela '{table_name}'.")
+
 except Exception as e:
-    print(f"❌ Erro na conexão: {e}")
+    print(f"❌ Erro na conexão ou na inserção dos dados: {e}")
+
+
